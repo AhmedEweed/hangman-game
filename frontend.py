483151828d83984aaa0c3f92440ce98befeb5dad
Ahmed Eweed <xmega.mind10@gmail.com>
2019-10-28 19:10:45 +0200

    feat: make a user interface

---
+++ frontend.py
@@ -0,0 +1,12 @@
+import wx
+import hangman
+app = wx.App()
+frame = wx.Frame(parent = None, title = "Guessing Game")
+panel = wx.Panel(frame)
+sizer = wx.GridBagSizer()
+
+
+input_label = 
+
+frame.Show()
+app.MainLoop()
